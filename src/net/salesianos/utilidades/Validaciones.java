a8923f921ec3791e98eb7f18c7e57255ddfe4a72
J44v11 <javiergarcialopez101@gmail.com>
2026-05-14 18:46:35 +0100

    feat: agrega método solicitarDouble en la clase Validaciones

--- src/net/salesianos/utilidades/Validaciones.java
+++ src/net/salesianos/utilidades/Validaciones.java
@@ -14,4 +14,16 @@ public class Validaciones {
             }
         }
     }
+
+    public static double solicitarDouble(String mensaje) {
+        while (true) {
+            try {
+                String input = JOptionPane.showInputDialog(mensaje);
+                if (input == null) return -1.0;
+                return Double.parseDouble(input.replace(",", ".")); // Por si el usuario usa coma, las reemplazamos por punto
+            } catch (NumberFormatException e) {
+                JOptionPane.showMessageDialog(null, "Se ha cometido un error. Debes introducir un numero decimal valido.");
+            }
+        }
+    }
 }
