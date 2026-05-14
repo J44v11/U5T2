4826305d50d23b75ee952dc2bdbacbd10a5db016
J44v11 <javiergarcialopez101@gmail.com>
2026-05-14 18:45:05 +0100

    feat: implementa método solicitarEntero en la clase Validaciones

--- src/net/salesianos/utilidades/Validaciones.java
+++ src/net/salesianos/utilidades/Validaciones.java
@@ -1,5 +1,17 @@
 package net.salesianos.utilidades;
+import javax.swing.JOptionPane;
 
 public class Validaciones {
     
+    public static int solicitarEntero(String mensaje) {
+        while (true) {
+            try {
+                String input = JOptionPane.showInputDialog(mensaje);
+                if (input == null) return -1; 
+                return Integer.parseInt(input); 
+            } catch (NumberFormatException e) {
+                JOptionPane.showMessageDialog(null, "Se ha cometido un error. Debes introducir un numero entero.");
+            }
+        }
+    }
 }
